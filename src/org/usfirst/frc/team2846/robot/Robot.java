be59316d3d7dc5f0a87993e95e55d69c95408227
Keith <Keith@10.30.9.78>
2017-01-15 14:00:09 -0600

    Changed labels

--- src/org/usfirst/frc/team2846/robot/Robot.java
+++ src/org/usfirst/frc/team2846/robot/Robot.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Simple program for testing the navX-MXP Board on our robot.
+ * The navX values will be displayed on the SmartDashboard while the
+ * robot is enabled.
  */
 public class Robot extends IterativeRobot {
 
@@ -35,22 +37,27 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
+		navXBoard.reset();
 		SmartDashboard.putString("angle", navXBoard.getFirmwareVersion());
 	}
 	
 	@Override
 	public void teleopPeriodic() {
-		SmartDashboard.putNumber("angle", navXBoard.getAngle());
-		SmartDashboard.putNumber("compass", navXBoard.getCompassHeading());
-		SmartDashboard.putNumber("pitch", navXBoard.getPitch());
-		SmartDashboard.putNumber("roll", navXBoard.getRoll());
-		SmartDashboard.putNumber("yaw", navXBoard.getYaw());
-		SmartDashboard.putNumber("DisplacementX", navXBoard.getDisplacementX());
-		SmartDashboard.putNumber("DisplacementY", navXBoard.getDisplacementY());
-		SmartDashboard.putNumber("DisplacementZ", navXBoard.getDisplacementZ());
-		SmartDashboard.putNumber("VelocityX", navXBoard.getVelocityX());
-		SmartDashboard.putNumber("VelocityY", navXBoard.getVelocityY());
-		SmartDashboard.putNumber("VelocityZ", navXBoard.getVelocityZ());
+		SmartDashboard.putNumber("navX.angle", navXBoard.getAngle());
+		SmartDashboard.putNumber("navX.compass", navXBoard.getCompassHeading());
+		SmartDashboard.putNumber("navX.pitch", navXBoard.getPitch());
+		SmartDashboard.putNumber("navX.roll", navXBoard.getRoll());
+		SmartDashboard.putNumber("navX.yaw", navXBoard.getYaw());
+		SmartDashboard.putNumber("navX.DisplacementX", navXBoard.getDisplacementX());
+		SmartDashboard.putNumber("navX.DisplacementY", navXBoard.getDisplacementY());
+		SmartDashboard.putNumber("navX.DisplacementZ", navXBoard.getDisplacementZ());
+		SmartDashboard.putNumber("navX.VelocityX", navXBoard.getVelocityX());
+		SmartDashboard.putNumber("navX.VelocityY", navXBoard.getVelocityY());
+		SmartDashboard.putNumber("navX.VelocityZ", navXBoard.getVelocityZ());
+		SmartDashboard.putBoolean("navX.isConnected", navXBoard.isConnected());
+		SmartDashboard.putBoolean("navX.isMoving", navXBoard.isMoving());
+		SmartDashboard.putBoolean("navX.isRotating", navXBoard.isRotating());
+		
 	}
 
 	@Override
